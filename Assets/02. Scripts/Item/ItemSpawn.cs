c02145feb7a09a3ef5ff66e254baa98fac6a243b
g1-1g <111170307+g1-1g@users.noreply.github.com>
2025-11-07 15:01:54 +0900

    refact : 아이템 스폰 확률 방식 수정
    가중치 방식

--- Assets/02. Scripts/Item/ItemSpawn.cs
+++ Assets/02. Scripts/Item/ItemSpawn.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public enum EItemType
@@ -11,30 +13,35 @@ public class ItemSpawn : MonoBehaviour
 {
     public GameObject[] Item;
 
-    public float ProbabilityAttackSpeedUp = 0.1f;
-    public float ProbabilityMoveSpeedUp = 0.2f;
-    public float ProbabilityHealthUp = 0.7f;
 
+
+
+    public float[] ItemWeight = { 10f, 20f, 30f };
+    private float totalWeight;
+
+    public void Start()
+    {
+        totalWeight = ItemWeight.Sum();
+
+    }
 
     public void SpawnItem(Vector3 position)
     {
 
-        float rand = UnityEngine.Random.Range(0, 1.0f);
-        if (rand <= 0.5f)
-        {
-            return;
-        }
+
+        if (UnityEngine.Random.Range(0, 1.0f) <= 0.5f) return;
+
         GameObject item = ChooseItem();
         Instantiate(item, position, Quaternion.identity);
     }
 
     private GameObject ChooseItem()
     {
-        float rand = UnityEngine.Random.Range(0, 1.0f);
-        if (rand <= ProbabilityHealthUp)
+        float rand = UnityEngine.Random.Range(0, 100);
+        if (rand <= ItemWeight[(int)EItemType.HealthUp]/ totalWeight*100)
         {
             return Item[(int)EItemType.HealthUp];
-        } else if (ProbabilityHealthUp < rand && rand <= ProbabilityHealthUp + ProbabilityMoveSpeedUp)
+        } else if (rand <= ItemWeight[(int)EItemType.HealthUp] / totalWeight * 100 + ItemWeight[(int)EItemType.MoveSpeedUp] / totalWeight * 100)
         {
             return Item[(int)EItemType.MoveSpeedUp];
         }
