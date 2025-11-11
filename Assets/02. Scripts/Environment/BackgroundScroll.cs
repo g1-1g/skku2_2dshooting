dc36029ba1af2ed6979ff798bbeaa5cbabc5b172
g1-1g <111170307+g1-1g@users.noreply.github.com>
2025-11-11 13:51:42 +0900

    feat : 배경 시차 scrolling 적용

--- Assets/02. Scripts/Environment/BackgroundScroll.cs
+++ Assets/02. Scripts/Environment/BackgroundScroll.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class BackgroundScroll : MonoBehaviour
 {
-   
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public Material BackgroundMaterial;
+    public float scrollSpeed = 0.1f;
+
+    private void Start()
     {
-        
+        BackgroundMaterial = GetComponent<Renderer>().material;
+    }
+    private void Update()
+    {
+        Vector2 direction = Vector2.up;
+        BackgroundMaterial.mainTextureOffset += direction * scrollSpeed * Time.deltaTime;
     }
 }
